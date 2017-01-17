40ce8b0423e4b1b70d11cb1c3eaa3d9b041a9d46
Zijing Liu <zijingli@usc.edu>
2017-01-17 13:40:40 -0800

    Setup 445 folder

---
+++ Intro_Robotics/labs/lab1/lab1.py
@@ -0,0 +1,33 @@
+"""
+Example to move robot forward for 10 seconds
+Use "python3 run.py [--sim] example1" to execute
+"""
+
+
+class Run:
+    def __init__(self, factory):
+        """Constructor.
+
+        Args:
+            factory (factory.FactoryCreate)
+        """
+        self.create = factory.create_create()
+        self.time = factory.create_time_helper()
+
+    def run(self):
+        self.create.start()
+        self.create.safe()
+
+        self.create.drive_direct(100, 100)
+        self.time.sleep(5)
+        self.create.drive_direct(100, -100)
+        self.time.sleep(5)
+        self.create.drive_direct(-100, 100)
+        self.time.sleep(5)
+        self.create.drive_direct(50, 100)
+        self.time.sleep(5)
+        self.create.drive_direct(-100, -100)
+        self.time.sleep(5)
+        self.create.drive_direct(0, 0)
+
+        self.create.stop()
